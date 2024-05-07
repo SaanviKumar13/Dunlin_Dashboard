51b9711ecd5046427824e737185e0ca17a01b877
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-05-07 23:40:39 +0530

    fix: shift component to transactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import AppLayout from './components/AppLayout';
-import PageNotFound from './pages/PageNotFound';
-import UploadCsv from './components/UploadCsv';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import UploadCsv from "./components/UploadCsv";
 import Signin from "./pages/SignIn";
 import Login from "./pages/Login";
 
@@ -9,12 +9,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Signin />} />
-        <Route path='/login' element={<Login/> } />
+        <Route path="/" element={<Signin />} />
+        <Route path="/login" element={<Login />} />
         <Route element={<AppLayout />}>
-          <Route path='/uploadcsv' element={<UploadCsv />} />
+          <Route path="/uploadcsv" element={<UploadCsv />} />
+          <Route path="/upload" element={<UploadCsv />} />
         </Route>
-        <Route path='*' element={<PageNotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
